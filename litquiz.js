6c0ca6855bb7dae06994c65f383191d72a1a1f5a
Nathan Daniel Peterson <natperson@gmail.com>
2017-12-09 13:52:33 -0800

    questions are now shuffled instead of randomized

--- litquiz.js
+++ litquiz.js
@@ -1,203 +1,64 @@
-let score = 0;
+let score = 0
 const scoreDisplay = document.querySelector("#scoreDisplay")
 const question = document.querySelector("#question")
 const choices = document.querySelectorAll(".choices")
 const background = document.querySelector('.container')
+let gameArray = shuffleQuestions()
+let counter = gameArray.length - 1
 
-const setBackgroundColor = function(e){
-  if(e){
-    background.style.background = 'yellow'
-  } else {
-    background.style.background = 'orange'
+function shuffleQuestions(){
+  let shuffledArray = []
+  let length = questions.length
+  let index
+  while(length){
+    index = Math.floor(Math.random() * length)
+    shuffledArray.push(questions.splice(index, 1))
+    length--
   }
-  setTimeout(resetBackgroundColor, 1000)
-}
-
-const resetBackgroundColor = function(){
-  background.style.background = '#62B6CB'
+  return shuffledArray
 }
 
 $(".answers").click(function(e) {
   var classes = e.target.classList.value;
   if (classes.includes("correct")) {
-    score += 100;
-    setBackgroundColor(e);
+    score += 100
     $(function() {
       $('#yes').fadeIn('normal', function() {
       $(this).delay(100).fadeOut();
-   });
-});
+   })
+})
   } else if (classes.includes("wrong")) {
 $(function() {
       $('#no').fadeIn('normal', function() {
       $(this).delay(100).fadeOut();
-   });
-});
-    score -= 50;
-      setBackgroundColor();
+   })
+})
+    score -= 50
   }
-  scoreDisplay.textContent = score;
-  reset();
-});
+  counter--
+  scoreDisplay.textContent = score
+  counter > 0 ? reset(gameArray.shift()) : endGame()
+})
+
 
 //randomly pick next question from questions & display question
-function reset() {
-  var nextQNum = Math.floor(Math.random() * questions.length);
-  var nextQ = questions[nextQNum].question;
-  $("#q").html(questions[nextQNum].question);
-  $("#one").html(questions[nextQNum].choices[0]);
-  $("#two").html(questions[nextQNum].choices[1]);
-  $("#three").html(questions[nextQNum].choices[2]);
-  $("#four").html(questions[nextQNum].choices[3]);
-  addClasses(nextQNum);
+function reset(question) {
+  question = question[0]
+  $("#question").html(question.question)
+  $("#one").html(question.choices[0])
+  $("#two").html(question.choices[1])
+  $("#three").html(question.choices[2])
+  $("#four").html(question.choices[3])
+  addClasses(question)
 }
 // add correct class to
-function addClasses(index) {
-  var multChoices = questions[index].choices;
-  var answerInd = questions[index].correct;
-  var answer = questions[index].choices[answerInd];
-
-  for (i = 0; i < multChoices.length; i++) {
-    choices[i].classList = ["choices"];
-    if (answerInd === i) choices[i].classList.add("correct");
-    else choices[i].classList.add("wrong");
+function addClasses(question) {
+  for (i = 0; i < question.choices.length; i++) {
+    choices[i].classList = ["choices"]
+    question.correct === i ? choices[i].classList.add("correct") : choices[i].classList.add("wrong")
   }
 }
 
-const questions = [
-  {
-    question: "Who is the author of <em>A Tale of Two Cities</em>?",
-    choices: [
-      "Charles Dickens",
-      "Jane Austen",
-      "Daniel Defoe",
-      "Arthur Conan Doyle"
-    ],
-    correct: 0
-  },
-  {
-    question:
-      "Who is the narrator of F. Scott Fitzgerald's <em>The Great Gatsby</em>?",
-    choices: [
-      "F. Scott Fitzgerald",
-      "Nick Carraway",
-      "Jay Gatsby",
-      "T.J. Eckleburg"
-    ],
-    correct: 1
-  },
-  {
-    question: "What is Hamlet's mother's name?",
-    choices: ["Penelope", "Gertrude", "Ophelia", "Calliope"],
-    correct: 1
-  },
-  {
-    question:
-      "Identify the source of these lines: 'Beauty is truth, truth beauty, that is all / Ye know on earth, and all ye need to know.'",
-    choices: [
-      "John Keats's 'Ode To a Grecian Urn'",
-      "Shakespeare's <em>The Tempest</em>",
-      "Edgar Allan Poe's 'Ligeia'",
-      "Sir Philip Sidney's 'Sonnet 64'"
-    ],
-    correct: 0
-  },
-  {
-    question:
-      "When was the oldest surviving copy of Homer's <em>Iliad</em> composed?",
-    choices: [
-      "From the ninth to the fifteenth century CE",
-      "117 BCE",
-      "Eight century BCE",
-      "Ninth century BCE"
-    ],
-    correct: 2
-  },{
-    question:
-      "How many novels did Tobias Smollett write?",
-    choices: [
-      "1",
-      "6",
-      "16",
-      "61"
-    ],
-    correct: 1
-  },
-    {
-    question:
-      "Who is the author of the following lines:<br>'And we are here as on a darkling plain<br>Swept with confused alarms of struggle and flight,<br>Where ignorant armies clash by night?'",
-    choices: [
-      "Seamus Heaney",
-      "Anne Sexton",
-      "Lord Alfred Tennyson",
-      "Matthew Arnold"
-    ],
-    correct: 3
-  },
-  {
-    question:
-      "Which of the following best describes blank verse?",
-    choices: [
-      "Unrhymed iambic pentameter",
-      "Rhymed iambic tetrameter",
-      "Free verse",
-      "Accented verse"
-    ],
-    correct: 0
-  },
-  {
-    question:
-      "How old was Miguel Cervantes when he completed <em>Don Quixote</em>?",
-    choices: [
-      "27",
-      "37",
-      "42",
-      "68"
-    ],
-    correct: 3
-  },
-  {
-    question:
-      "Theocritus is known for his contribution to which major ancient genre?",
-    choices: [
-      "Pastoral",
-      "Epic",
-      "Tragedy",
-      "Biography"
-    ],
-    correct: 0
-  },
-    {
-    question:
-      "Who first used the phrase, 'To err is human, to forgive divine?'",
-    choices: [
-      "William Shakespeare",
-      "Alexander Pope",
-      "William Wordsworth",
-      "Emily Dickinson"
-    ],
-    correct: 1
-  },
-  {
-    question:
-      "Which of these poems is the shortest (in lines)?",
-    choices: [
-      "Ezra Pound, 'In a Station of the Metro",
-      "William Carlos Williams, 'The Red Wheelbarrow'",
-      "Basho, 'An old silent pond'",
-      "Marianne Moore, 'Silence'"
-    ],
-    correct: 0
-  },
-  {
-    question:
-      "Lyn Hejinian is known for her contribution to which major twentieth century movement?",
-    choices: [
-      "Postmodernism",
-      "New Formalism",
-      "Language Poetry",
-      "Martian Poetry"
-    ],
-    correct: 2
-  },
-];
+function endGame() {
+  background.innerHTML = `<h3>Game Over</h3><p>Your score was ${score}</p><p>Thanks for playing!</p>`
+}
---
+++ questions.js
@@ -0,0 +1,234 @@
+const emptyQuestion = {
+  question:
+    "",
+  choices: [
+    ""
+  ],
+  correct: 0
+}
+
+const questions =
+[
+  {
+    question: "Who is the author of <em>A Tale of Two Cities</em>?",
+    choices: [
+      "Charles Dickens",
+      "Jane Austen",
+      "Daniel Defoe",
+      "Arthur Conan Doyle"
+    ],
+    correct: 0
+  },
+  {
+    question:
+      "Who is the narrator of F. Scott Fitzgerald's <em>The Great Gatsby</em>?",
+    choices: [
+      "F. Scott Fitzgerald",
+      "Nick Carraway",
+      "Jay Gatsby",
+      "T.J. Eckleburg"
+    ],
+    correct: 1
+  },
+  {
+    question: "What is Hamlet's mother's name?",
+    choices: ["Penelope", "Gertrude", "Ophelia", "Calliope"],
+    correct: 1
+  },
+  {
+    question:
+      "Identify the source of these lines: 'Beauty is truth, truth beauty, that is all / Ye know on earth, and all ye need to know.'",
+    choices: [
+      "John Keats's 'Ode To a Grecian Urn'",
+      "Shakespeare's <em>The Tempest</em>",
+      "Edgar Allan Poe's 'Ligeia'",
+      "Sir Philip Sidney's 'Sonnet 64'"
+    ],
+    correct: 0
+  },
+  {
+    question:
+      "When was the oldest surviving copy of Homer's <em>Iliad</em> composed?",
+    choices: [
+      "From the ninth to the fifteenth century CE",
+      "117 BCE",
+      "Eight century BCE",
+      "Ninth century BCE"
+    ],
+    correct: 2
+  },{
+    question:
+      "How many novels did Tobias Smollett write?",
+    choices: [
+      "1",
+      "6",
+      "16",
+      "61"
+    ],
+    correct: 1
+  },
+    {
+    question:
+      "Who is the author of the following lines:<br>'And we are here as on a darkling plain<br>Swept with confused alarms of struggle and flight,<br>Where ignorant armies clash by night?'",
+    choices: [
+      "Seamus Heaney",
+      "Anne Sexton",
+      "Lord Alfred Tennyson",
+      "Matthew Arnold"
+    ],
+    correct: 3
+  },
+  {
+    question:
+      "Which of the following best describes blank verse?",
+    choices: [
+      "Unrhymed iambic pentameter",
+      "Rhymed iambic tetrameter",
+      "Free verse",
+      "Accented verse"
+    ],
+    correct: 0
+  },
+  {
+    question:
+      "How old was Miguel Cervantes when he completed <em>Don Quixote</em>?",
+    choices: [
+      "27",
+      "37",
+      "42",
+      "68"
+    ],
+    correct: 3
+  },
+  {
+    question:
+      "Theocritus is known for his contribution to which major ancient genre?",
+    choices: [
+      "Pastoral",
+      "Epic",
+      "Tragedy",
+      "Biography"
+    ],
+    correct: 0
+  },
+    {
+    question:
+      "Who first used the phrase, 'To err is human, to forgive divine?'",
+    choices: [
+      "William Shakespeare",
+      "Alexander Pope",
+      "William Wordsworth",
+      "Emily Dickinson"
+    ],
+    correct: 1
+  },
+  {
+    question:
+      "Which of these poems is the shortest (in lines)?",
+    choices: [
+      "Ezra Pound, 'In a Station of the Metro",
+      "William Carlos Williams, 'The Red Wheelbarrow'",
+      "Basho, 'An old silent pond'",
+      "Marianne Moore, 'Silence'"
+    ],
+    correct: 0
+  },
+  {
+    question:
+      "Lyn Hejinian is known for her contribution to which major twentieth century movement?",
+    choices: [
+      "Postmodernism",
+      "New Formalism",
+      "Language Poetry",
+      "Martian Poetry"
+    ],
+    correct: 2
+  },{
+  question :
+    "When the first edition of this poet's works appeared in 1855, the <em> Boston Intelligencer</em> said in its review: 'The author should be kicked out from all decent society as below the level of the brute. He must be some escaped lunatic raving in pitable delerium.' Who was it?",
+    choices: [
+      "Edgar Allan Poe",
+      "Walt Whitman",
+      "Ralph Waldo Emerson",
+      "John Clare"
+    ],
+    correct: 1
+  },
+  {
+    question:
+      "When this Irish playwright was stopped by U.S. customs in New York, he proclaimed, 'I have nothing to declare except my genius.",
+    choices: [
+      "Samuel Beckett",
+      "George Bernard Shaw",
+      "Brian Friel",
+      "Oscar Wilde"
+    ],
+    correct: 3
+  },
+  {
+    question:
+      "What is the real name of the author of <em>Middlemarch</em>?",
+    choices: [
+      "Mary Ann Evans",
+      "George Eliot",
+      "Harriet Carker",
+      "Susanna O'Byrne"
+    ],
+    correct: 0
+  },
+  {
+    question:
+      "What is the lock in Alexander Pope's 'The Rape of the Lock'?",
+    choices: [
+      "The gate to a woman's chamber",
+      "A section of a canal",
+      "A length of hair",
+      "A pistol"
+    ],
+    correct: 3
+  },
+  {
+    question:
+      "What is the name of the main character in H.G. Wells's <em>The Invisible Man</em>?",
+    choices: [
+      "Griffin",
+      "Baron",
+      "Solomon",
+      "Evans"
+    ],
+    correct: 0
+  },
+  {
+    question:
+      "What does <em>R.U.R.</em> stand for in Karl Capek's play?",
+    choices: [
+      "Raliegh University Regents",
+      "Rossum's Universal Robots",
+      "Randomized Unpredictable Rain",
+      "Russian Underground Railroad"
+    ],
+    correct: 1
+  },
+  {
+    question:
+      "The following sentence is the first line of which novel: 'It was a bright cold day in April, and the clocks were strinking thirteen.'",
+    choices: [
+      "Italo Calvino's <em>Cosmicomics</em>",
+      "Thomas Pynchon's <em>Gravity's Rainbow</em>",
+      "Virginia Woolf's <em>To The Lighthouse</em>",
+      "George Orwell's <em>1984</em>"
+    ],
+    correct: 3
+  },
+  {
+    question:
+      "Which is the longest of William Shakespeare's plays?",
+    choices: [
+      "<em>The Comedy of Errors</em>",
+      "<em>King Lear</em>",
+      "<em>Hamlet</em>",
+      "<em>Henry VIII</em>"
+    ],
+    correct: 2
+  }
+]
